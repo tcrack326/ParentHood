18c9d4a410b94ae76bd76c828a6bb3805ec2ffac
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-12-02 16:03:32 -0500

    work on voting and answers

--- app/scripts/questions/single-question-controller.js
+++ app/scripts/questions/single-question-controller.js
@@ -4,50 +4,74 @@
     .controller('SingleQuestionController', ['questionsFactory', 'usersFactory', 'answersFactory', '$scope', '$routeParams', '$rootScope', '$location', function (questionsFactory, usersFactory, answersFactory, $scope, $routeParams, $rootScope, $location) {
 
     usersFactory.checkUser();
+
     $scope.logOutUser = function () {
       usersFactory.logOutUser();
     };
 
-
     questionsFactory.getQuestion($routeParams.id).then(function (data){
       //get the question and bind it to scope
-    $scope.question = data;
+      $scope.question = data;
+
       //add to the total view count
-    $scope.question.totalViews++;
-    $scope.updateQuestion($scope.question);
-    answersFactory.getAnswers().then( function(answers) {
+      $scope.question.totalViews++;
+      $scope.updateQuestion($scope.question);
 
-      var answersForQuestion = _.where(answers, { question: $scope.question.objectId });
+      //get the answers to the question
+      answersFactory.getAnswers().then( function (answers) {
 
-      $scope.answers = answersForQuestion;
-    });
+        var answersForQuestion = _.where(answers, { question: $scope.question.objectId });
 
-    //hide buttons if User is not logged in and if not the author of the question
-    if(Parse.User.current() != undefined) {
+        $scope.answers = answersForQuestion;
+
+        //hide the delete button if answer's author id is different than current user's
+        _.each($scope.answers, function(answer){
+          if (Parse.User.current() != undefined) {
+            if (Parse.User.current().id === answer.user) {
+              console.log(0);
+              $('#'+ answer.objectId + '-' + answer.user).show();
+
+            }
+            else {
+              console.log(1);
+              $('#'+ answer.objectId + '-' + answer.user).hide();
+            }
+          }
+          else {
+
+            $('.deleteAnswer').hide();
+            $('.voteUp').hide();
+            $('.voteDown').hide();
+            $('.isAnswer').hide();
+
+          }
+          $scope.$apply();
+        });
+
+      });
 
 
+      //hide buttons if User is not logged in and if not the author of the question
+      if (Parse.User.current() != undefined) {
 
-      if(Parse.User.current().id === $scope.question.user) {
+        if (Parse.User.current().id === $scope.question.user) {
+          $('#editQuestion').show();
+          $('#deleteQuestion').show();
+        }
 
-        $('#editQuestion').show();
-        $('#deleteQuestion').show();
+        else {
+          $('#editQuestion').hide();
+          $('#deleteQuestion').hide();
+        }
+
       }
-
       else {
+        $('#answerArea').hide();
         $('#editQuestion').hide();
         $('#deleteQuestion').hide();
       }
 
-    }
-    else {
-      $('#answerQuestion').hide();
-      $('#editQuestion').hide();
-      $('#deleteQuestion').hide();
-    }
-
   });
-
-
 
 
   $scope.updateQuestion = function (question) {
@@ -78,7 +102,32 @@
     answer.votes = 0;
     answer.isAnswer = false;
     answersFactory.addAnswer(answer);
-    $scope.$apply();
+
+    $rootScope.$on('answer:added', function (){
+
+      answersFactory.getAnswers().then( function (answers) {
+
+        var answersForQuestion = _.where(answers, { question: $scope.question.objectId });
+
+        $scope.answers = answersForQuestion;
+
+      });
+
+      //$scope.answers.push(answer);
+
+      //$scope.$apply();
+      $('#answerBox').val('');
+
+    });
+
+  }
+
+  $scope.deleteAnswer = function (answer) {
+    answersFactory.deleteAnswer(answer);
+
+    $rootScope.$on('answer:deleted', function (){
+      $scope.answers = _.without($scope.answers, answer);
+    });
   }
 
   $scope.voteUp = function (answer) {
