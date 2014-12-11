c06fd7fc92f7b85535f262ffd2eddfc02d6f7a19
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-12-11 15:45:22 -0500

    completed

--- app/scripts/users/user-profile.js
+++ app/scripts/users/user-profile.js
@@ -4,6 +4,7 @@
     .controller('ProfileUserController', ['usersFactory', 'userStatsFactory', '$scope', '$rootScope', '$location', '$routeParams', function (usersFactory, userStatsFactory, $scope, $rootScope, $location, $routeParams) {
 
     $('#navModal').foundation('reveal', 'close');
+    $scope.isEditing = false;
 
     usersFactory.getUser($routeParams.id).then( function(data) {
       $scope.user = data;
@@ -11,6 +12,37 @@
     userStatsFactory.getUserStats($routeParams.id).then( function(data) {
       $scope.userStats = data[0];
     });
+
+    $scope.beginEditing = function () {
+      $scope.isEditing = true;
+    }
+
+    $scope.updateUserStats = function (userStats) {
+
+      userStatsFactory.updateUserStats(userStats);
+
+      $rootScope.$on('userStats:updated', function (){
+        $location.path('/');
+      });
+
+    }
+
+    $scope.userAuth = function () {
+
+        if (Parse.User.current() != undefined) {
+          if (Parse.User.current().id === $scope.user.objectId) {
+            return true;
+          }
+          else {
+            return false;
+          }
+        }
+        else {
+          return false;
+        }
+
+    }
+
 
 
     }]);
